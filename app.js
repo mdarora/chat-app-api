2aa304c7b18210b5885e0e37af7d0e924449e156
MD Arora <mdarora0001@gmail.com>
2021-06-06 16:02:11 +0530

    parsing cookies recieved from client

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 dotenv.config({path:".env"});
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(require("./router"));
 
 
